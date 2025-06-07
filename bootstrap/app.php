cf9a6ad44b199542393f652a2249872d76b90d5b
Muktar Usman <mualiyuoox@gmail.com>
2025-06-07 04:29:53 +0100

    First Commit of the day

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
         $middleware->validateCsrfTokens(except: [
         'https://api.nutry-lyfe.pmall.com.ng/*',
+        'http://api.nutry-lyfe.pmall.com.ng/*',
         // 'http://example.com/foo/*',
     ]);
     })
